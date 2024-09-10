be351671e12750375ab22493bb1aa10d4f72af82
Breno Pais <brenopais7@gmail.com>
2024-09-10 17:49:27 -0300

    alterando imagem

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ function toggleMode() {
   if (html.classList.contains('light')) {
     img.setAttribute("src", "./assets/Perfilbreno.png");
   } else {
-    img.setAttribute("src", "./assets/Perfilbrenob.png");
+    img.setAttribute("src", "./assets/Perfilbreno.png");
   }
 }
